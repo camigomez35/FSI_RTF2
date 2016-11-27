9c6a907d418166adc0d811cbcad01de080daf988
yoiner.gomez <yoiner.gomez22@gmail.com>
2016-11-27 10:44:42 -0500

    Corrigiendo la generacion de reportes

--- src/java/co/edu/udea/fsi/rt1/controlador/ReportController.java
+++ src/java/co/edu/udea/fsi/rt1/controlador/ReportController.java
@@ -22,7 +22,7 @@ import net.sf.jasperreports.engine.util.JRLoader;
  */
 public class ReportController {
     
-    private int idAeropuerto;
+    private String idAeropuerto;
     public Connection conn ;
     /**
      * Creates a new instance of ReportController
@@ -31,54 +31,60 @@ public class ReportController {
         try{
             Class.forName("com.mysql.jdbc.Driver").newInstance(); //se carga el driver
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/softfly", "root", "root");
-            System.out.println("@@@@@@@@@@@@Conexion establecida");
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
-
+    
+    public void listFilesForFolder(final File folder) {
+                for (final File fileEntry : folder.listFiles()) {
+                    if (fileEntry.isDirectory()) {
+                        listFilesForFolder(fileEntry);
+                    } else {
+                        System.out.println(fileEntry.getName());
+                    }
+                }
+    }
+    
     public void reportar(){
-        System.out.println("@@@@@@@@@@@@"+conn);
-         try {
+        JasperReport masterReport = null;
+        try{
             String archivo = "AeropuertoRutaReporte.jasper";
-            System.out.println("Cargando desde: " + archivo);
             if (archivo == null) {
                 System.out.println("No se encuentra el archivo.");
                 System.exit(2);
             }
-            File ruta = new File("/AeropuertoRutaReporte.jasper");
-            JasperReport masterReport = null;
+            ///home/esteban/NetBeansProjects/FSI_RTF2/src/java/co/edu/udea/fsi/rt1/controlador/AeropuertoRutaReporte.jasper
+            System.out.println(new File(".").getCanonicalPath());
+            String ruta = new File(".").getCanonicalPath()+"/src/java/co/edu/udea/fsi/rt1/controlador/AeropuertoRutaReporte.jasper";
+            ruta = "/home/esteban/NetBeansProjects/FSI_RTF2/src/java/co/edu/udea/fsi/rt1/controlador/AeropuertoRutaReporte.jasper";
+            
             try {
-                masterReport = (JasperReport) JRLoader.loadObjectFromFile("/home/estudiantelis/Escritorio/FSI/RTF01_FSI_2016/src/java/co/edu/udea/fsi/rt1/controlador/AeropuertoRutaReporte.jasper");
-                System.out.println("@@@@Miedos");
+                //masterReport = (JasperReport) JRLoader.loadObjectFromFile("/home/esteban/NetBeansProjects/FSI_RTF2/src/java/co/edu/udea/fsi/rt1/controlador/AeropuertoRutaReporte.jasper");
+             masterReport = (JasperReport) JRLoader.loadObject(new File(ruta));
             } catch (JRException e) {
                 System.out.println("Error cargando el reporte maestro: " + e.getMessage());
                 System.exit(3);
             }
-//este es el parámetro, se pueden agregar más parámetros
-//basta con poner mas parametro.put
             Map parametro = new HashMap();
-            parametro.put("idAeropuerto", idAeropuerto);//Reporte diseñado y compilado con iReport
-            //JasperPrint jasperPrint = JasperFillManager.fillReport(masterReport, parametro, conn);
-            String reporte = JasperFillManager.fillReportToFile("/home/estudiantelis/Escritorio/FSI/RTF01_FSI_2016/src/java/co/edu/udea/fsi/rt1/controlador/AeropuertoRutaReporte.jasper", parametro, conn);
-            System.out.println("@@@@Miedo2"+ parametro);
-             JasperExportManager.exportReportToPdfFile(reporte,"/home/estudiantelis/Escritorio/sample_report.pdf");
-//Se lanza el Viewer de Jasper, no termina aplicación al salir
-//            JasperViewer jviewer = new JasperViewer(jasperPrint, false);
-//            System.out.println("@@@@Miedo3");
-//            jviewer.setTitle("SoftFly -Reporte");
-//            jviewer.setVisible(true);
+            parametro.put("idAeropuerto", Integer.parseInt(idAeropuerto));//Reporte diseñado y compilado con iReport
+            String reporte = JasperFillManager.fillReportToFile(ruta, parametro, conn);
+            JasperExportManager.exportReportToPdfFile(reporte,"/home/esteban/Escritorio/sample_report.pdf");
         } catch (Exception j) {
             System.out.println("Mensaje de Error: " + j.getMessage()+"QQ"+j.toString());
         }
     }
-    public int getIdAeropuerto() {
+
+    public String getIdAeropuerto() {
+        System.out.println("@@@ GET");
         return idAeropuerto;
     }
 
-    public void setIdAeropuerto(int idAeropuerto) {
+    public void setIdAeropuerto(String idAeropuerto) {
+        System.out.println("@@@ SET");
         this.idAeropuerto = idAeropuerto;
     }
+   
     
     
     
